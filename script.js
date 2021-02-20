a0fc7c072a1efcff8ead03196c5ced9ff3702912
paul-E404 <paul_engerling@web.de>
2021-02-20 12:48:40 +0100

    Final Responsive Design and Refacoring of javascript code

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 let right_answer;
 let question_number = 0;
 
-
-function hideElements() {
-    document.getElementById('correct-answer').classList.add('d-none');
-    document.getElementById('next-btn').classList.add('d-none');
-}
-
-
+/**
+ * Loads next questions by clicking on next-button.
+ */
 function nextQuestion() {
 
     hideElements();
@@ -17,13 +13,26 @@ function nextQuestion() {
 
 }
 
+/**
+ * Hides elements which are not longer needed.
+ */
+function hideElements() {
+    document.getElementById('correct-answer').classList.add('d-none');
+    document.getElementById('next-btn').classList.add('d-none');
+}
+
+/**
+ * Updates the progress bar.
+ */
 function showProgress() {
     let progress = Math.round((question_number / questions.length) * 100) + '%';
     document.getElementById('progress-bar').innerHTML = progress;
     document.getElementById('progress-bar').style.width = progress;
 }
 
-
+/**
+ * Shows the current question and answer options.
+ */
 function loadQuestion() {
     document.getElementById('question').innerHTML = questions[question_number - 1]['question'];
     document.getElementById('answer1').innerHTML = questions[question_number - 1]['answer_1'];
@@ -33,29 +42,40 @@ function loadQuestion() {
     right_answer = questions[question_number - 1]['right_answer'];
 }
 
-
+/**
+ * Shows the correct-answer- or wrong-answer-popup depending on the users choice.
+ * Shows the next-button if the answer is correct.
+ * @param  {number} a - Number 1 to 4 which stands for the right answer.
+ */
 function answer(a) {
     if (right_answer == a) {
         document.getElementById('correct-answer').classList.remove('d-none');
         document.getElementById('wrong-answer').classList.add('d-none');
         /* Show next button */
         document.getElementById('next-btn').classList.remove('d-none');
+        /* Finishes the quiz if the last answer is correct. */
+        if (question_number == questions.length) {
+            showProgress();
+            finishQuiz();
+        }
     } else {
         document.getElementById('correct-answer').classList.add('d-none');
         document.getElementById('wrong-answer').classList.remove('d-none');
     }
-    if (question_number == questions.length) {
-        showProgress();
-        finishQuiz();
-    }
 }
 
+/**
+ * Shows the final screen with congratulations.
+ */
 function finishQuiz() {
     document.getElementById('quiz-container').classList.add('d-none');
     document.getElementById('finished-container').classList.remove('d-none');
     animate();
 }
 
+/**
+ * Animates (enlarges) the winner image on the final screen.
+ */
 function animate() {
     setTimeout(function () {
         document.getElementById('img-winner').classList.add('enlarged');
@@ -66,6 +86,9 @@ function animate() {
     showRestartButton();
 }
 
+/**
+ * Pop-ups the restart button when the winner-image-animation finished.
+ */
 function showRestartButton() {
     setTimeout(function () {
         document.getElementById('restart-btn').classList.remove('d-none');
